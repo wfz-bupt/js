679ebc55928d4bd1349eefb145930bd817aa564c
wangfengzi <wangfengzi@bytedance.com>
2017-06-08 06:34:04 +0800

    ie中的范围

--- 12DOM2_DOM3.js
+++ 12DOM2_DOM3.js
@@ -112,6 +112,62 @@ startOffset: 范围在startContainer中起点的偏移量
 endContainer：包含范围终点的节点
 endOffset：范围在endContainer的偏移量
 commonAncestorContainer
+setStartBefore(refNode): 将范围的起点设置在refNode之前
+setStartAfter(refNode): 将范围的起点设置在refNode之后
+setEndBefore
+setEndAfter
+selectNode、selectNodeContents方法，例子
+以上两个方法，创建的range类型变量所具有的属性不同，详细看书。
+用dom范围实现复杂选择
+方法：setStart  setEnd
+接收两个参数，一个参照节点，和一个偏移量值
+选择例子中 llo到world中的o中间的dom部分，例子
+在创建范围时，会为范围创建一个文档片段，范围所属的所有节点被添加到这个
+片段中。如果范围内容不符合规范，就会自动补全缺失节点。
+创建了范围，就可以对范围进行操作，方法：
+deleteContents方法：删除范围包含的内容
+extractContents: 删除范围包含的内容，同时返回删除的文档片段
+cloneContents：复制并返回文档片段
+在将文档片段传入appendChild方法时，添加到文档中的只是片段的子节点
+insertNode: 向文档片段的开始处添加节点
+surroundContents: 环绕范围插入内容，参数为环绕节点
+5.折叠dom范围
+range.collapsed
+光标就是折叠的范围，因为范围里啥也没有，利用collapsed属性，可以确定
+两个节点是否紧密相邻
+6.比较dom范围
+compareBoundaryPoints,确定范围是否有公共的边界
+2个参数，比较方式，和另一个用来比较的范围
+7.复制dom范围
+cloneRange
+8.清理dom范围
+使用完范围后，要用detach方法，分离出该范围
+12.4.2 ie8及更早版本中的范围
+ie8及以前版本不支持dom范围。但是有一个类似功能的 文本范围(text range)
+可以通过body、button、input、textarea等几个元素的createTextRange
+方法创建文本范围。
+1.用ie范围实现简单的选择
+findText方法
+moveToElementText
+2.用ie范围实现复杂的选择
+move
+moveStart
+moveEng
+expand
+以上方法接收2个参数，移动单位，和移动的数量
+单位包括："character"、"word","sentence","textedit"
+3.操作ie范围中的内容
+parseHTML
+4.折叠ie范围
+collapse方法
+boundingWidth: 范围的宽度
+5.比较ie范围
+compareEndPoints
+isEqual方法
+inRange方法
+6复制ie范围
+duplicate方法
+
 */
 //监测浏览器是否支持某些dom模块
 var supportsDOM2Core = document.implementation.hasFeature("Core","2.0");
@@ -231,10 +287,30 @@ var walker =  document.createTreeWalker(div,NodeFilter.SHOW_ELEMENT,
 // 检测浏览器是否支持range接口
 var supportsRange = document.implementation.hasFeature('Range','2.0');
 var alsoSupportsRange = (typeof document.createRange == 'function');
-//一个标准的xhtml页面
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head></head>
-    <body></body>
+
+<html>
+    <body>
+        <p id="p1"><b>hello</b>world</p>
+    </body>
 </html>
+
+//利用以下代码来创建范围
+var range1 = document.createRange();
+var range2 = document.createRange();
+var p1 = document.getElementById("p1");
+range1.selectNode(p1); //包含p及所有子元素
+range2.selectNodeContents(p1);//只包含子元素
+
+var helloNode = p1.firstChild.firstChild;
+var worldNode = p1.lastChild;
+var range = document.createRange();
+range.setStart(helloNode,2);
+range.setEnd(worldNode,3);
+
+//一个标准的xhtml页面
+// <html xmlns="http://www.w3.org/1999/xhtml">
+//     <head></head>
+//     <body></body>
+// </html>
 
 
