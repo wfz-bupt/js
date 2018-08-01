5bc496d8d781d5096c1c95ac160ae08f9000c520
wangfz <wangfz@fenbi.com>
2018-08-01 16:59:05 +0800

    Revert "git"
    This reverts commit 8d3914c4053aac7fd60291979ecf85cf2eb07720.

--- faceTest/git.js
+++
@@ -1,7 +0,0 @@
-git reset --soft commitId
-撤销某个commit提交记录，保留本地文件的修改
-git reset --hard commitId
-撤销某个commit提交记录，不保留本地文件的修改，慎用
-git revert commitId
-将commitId的改动撤销，本地文件回退到commitid的状态。但是会产生新的commit记录，
-污染commit。
