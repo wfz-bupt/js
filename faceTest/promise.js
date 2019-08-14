33396f1bd9ec75237a49a05367569bd1767dfa61
wangfz <wangfz@fenbi.com>
2019-08-14 18:54:56 +0800

    MOD: promise

--- faceTest/promise.js
+++ faceTest/promise.js
@@ -20,4 +20,48 @@ function ajax () {
   })
 }
 
+// 自己实现一个简单的promise--支持链式调用
+function Promisewfz(fnc) {
+  const PENDING = Symbol()
+  const FULLFILLED = Symbol()
+  const REJECTED = Symbol()
+  let data = null
+  let state = PENDING
+  let handler = {}
+  function resolve(value) {
+    state = FULLFILLED
+    data = value
+    handler(data)
+  }
+  function reject(err) {
+    state = REJECTED
+    data = err
+    handler(data)
+  }
+  this.then = (cb) => {
+    if (state === FULLFILLED) {
+      cb(data)
+    }
+    if (state === PENDING) {
+      handler = cb
+    }
+  }
+  this.catch = function(cb) {
+    if (state === REJECTED) {
+      cb(data)
+    }
+    if (state === PENDING) {
+      handler = cb
+    }
+  }
+  fnc(resolve, reject)
+}
+let p = new Promisewfz((resolve, reject) => {
+  setTimeout(() => {
+    resolve('hello words')
+  }, 1000)
+})
+p.then(data => {
+  console.log(data)
+})
 // 给定一个promise数组，实现promise.all
